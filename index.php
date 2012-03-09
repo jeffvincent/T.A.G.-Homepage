7448412679ccfb95a4164407a91633769a0a1aef
Jeff Vincent <jeff@wistia.com>
2012-03-09 10:09:23 -0500

    arg I suck

--- index.php
+++ index.php
@@ -69,12 +69,12 @@ Shadowbox.init({
 </div>
 
 <div id="video_area">
-<div id="wistia_bba96803ed" style="width:640px;height:360px;" data-video-width="640" data-video-height="360"><object id="wistia_bba96803ed_seo" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" style="display:block;height:100%;position:relative;width:100%;"><param name="movie" value="http://embed.wistia.com/flash/embed_player_v2.0.swf?2012-02-08"></param><param name="allowfullscreen" value="true"></param><param name="allowscriptaccess" value="always"></param><param name="wmode" value="opaque"></param><param name="flashvars" value="videoUrl=http://embed.wistia.com/deliveries/f4e39dd1341074aeeff8bfa4addd23ace6029649.bin&&hdUrl=http://embed.wistia.com/deliveries/6d0bd0559f725e9a2025b1580aa78d0155c41615.bin&stillUrl=http://embed.wistia.com/deliveries/2cf10408a5149fc5b169eaa51780df28f8575167.bin"></param><embed src="http://embed.wistia.com/flash/embed_player_v2.0.swf?2012-02-08" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" wmode="opaque" flashvars="videoUrl=http://embed.wistia.com/deliveries/f4e39dd1341074aeeff8bfa4addd23ace6029649.bin&hdUrl=http://embed.wistia.com/deliveries/6d0bd0559f725e9a2025b1580aa78d0155c41615.bin&stillUrl=http://embed.wistia.com/deliveries/2cf10408a5149fc5b169eaa51780df28f8575167.bin" style="display:block;height:100%;position:relative;width:100%;"></embed></object></div>
+<div id="wistia_bba96803ed" style="width:500px;height:281px;" data-video-width="500" data-video-height="281"><object id="wistia_bba96803ed_seo" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" style="display:block;height:100%;position:relative;width:100%;"><param name="movie" value="http://embed.wistia.com/flash/embed_player_v2.0.swf?2012-02-08"></param><param name="allowfullscreen" value="true"></param><param name="allowscriptaccess" value="always"></param><param name="wmode" value="opaque"></param><param name="flashvars" value="videoUrl=http://embed.wistia.com/deliveries/f4e39dd1341074aeeff8bfa4addd23ace6029649.bin&&hdUrl=http://embed.wistia.com/deliveries/6d0bd0559f725e9a2025b1580aa78d0155c41615.bin&stillUrl=http://embed.wistia.com/deliveries/2cf10408a5149fc5b169eaa51780df28f8575167.bin"></param><embed src="http://embed.wistia.com/flash/embed_player_v2.0.swf?2012-02-08" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" wmode="opaque" flashvars="videoUrl=http://embed.wistia.com/deliveries/f4e39dd1341074aeeff8bfa4addd23ace6029649.bin&hdUrl=http://embed.wistia.com/deliveries/6d0bd0559f725e9a2025b1580aa78d0155c41615.bin&stillUrl=http://embed.wistia.com/deliveries/2cf10408a5149fc5b169eaa51780df28f8575167.bin" style="display:block;height:100%;position:relative;width:100%;"></embed></object></div>
 <script charset="ISO-8859-1" src="http://fast.wistia.com/static/E-v1.js"></script>
 <script>//<![CDATA[
 wistiaEmbed = Wistia.embed("bba96803ed", {
-  videoWidth: "640",
-  videoHeight: "360",
+  videoWidth: "500",
+  videoHeight: "281",
   controlsVisibleOnLoad: true,
   playerColor: "5b1211"
 });
