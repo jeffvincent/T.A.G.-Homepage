327e88d417888e048250f73583de8b811cf1ae20
Jeff Vincent <jeff@wistia.com>
2012-03-10 12:42:02 -0500

    DRYing up annoying code, implementing SASS

--- about.php
+++ about.php
@@ -9,7 +9,7 @@
 <?php include('includes/nav.php'); ?>
 
 <!--Bio Start-->
-<div id="bio_group" class="bio">
+<div id="bio_group">
 <div class="bio" id="chip">
 <div class="bio_pic">
 <img id="chip_pic" title="Chip - Lead Apple Guy" alt="Chip - Lead Apple Guy" src="images/about/chip.png" />
@@ -19,7 +19,7 @@
 <p>Lead Apple Guy
 <br>
 North Wales, PA</p>
-<blockquote>Imagine your computer broken. Now, imagine your computer working flawlessly. That's where we come into play.</blockquote>
+<blockquote class="info">Imagine your computer broken. Now, imagine your computer working flawlessly. That's where we come into play.</blockquote>
 
 <a id="video_bio_link_chip" href="http://fast.wistia.com/embed/iframe/a0e1077edb?videoWidth=720&videoHeight=405&controlsVisibleOnLoad=true&autoPlay=true&popover=true" class="wistia-popover[width=720,height=405,playerColor=#636155]"><img class="bio_video" title="Chip - Lead Apple Guy's Video Bio" alt="Chip - Lead Apple Guy's Video Bio" src="images/about/video.png" /></a><script charset="ISO-8859-1" src="http://fast.wistia.com/static/popover-v1.js"></script>
 </div></div>
@@ -32,7 +32,7 @@ North Wales, PA</p>
 <h2>Jay</h2>
 <p>Lead Apple Guy<br />West Chester, PA</p>
 
-<blockquote>Jaguar, Panther, Tiger, Leopard, Snow Leopard, Cougar, Bobcat, Cheetah, Lynx, Bengal - we'll fix whatever Apple throws our way.</blockquote>
+<blockquote class="info">Jaguar, Panther, Tiger, Leopard, Snow Leopard, Cougar, Bobcat, Cheetah, Lynx, Bengal - we'll fix whatever Apple throws our way.</blockquote>
 <a id="video_bio_link_jay" href="http://fast.wistia.com/embed/iframe/ea1af3c16e?videoWidth=702&videoHeight=395&controlsVisibleOnLoad=true&autoPlay=true&popover=true" class="wistia-popover[width=702,height=395,playerColor=#636155]"><img class="bio_video" title="Jay - Lead Apple Guy's Video Bio" alt="Jay - Lead Apple Guy's Video Bio" src="images/about/video.png" /></a><script charset="ISO-8859-1" src="http://fast.wistia.com/static/popover-v1.js"></script>
 </div></div>
 <div id="clear" style="clear:both;"></div>
