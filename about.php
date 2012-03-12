afef5d8d85b2ade322946aa948d7ad46cffb8c9c
Jeff Vincent <jeff@wistia.com>
2012-03-12 18:12:35 -0400

    ho hum still styling it up, about to tackle background re-design

--- about.php
+++ about.php
@@ -21,7 +21,7 @@
 North Wales, PA</p>
 <blockquote class="info">Imagine your computer broken. Now, imagine your computer working flawlessly. That's where we come into play.</blockquote>
 
-<a id="video_bio_link_chip" href="http://fast.wistia.com/embed/iframe/a0e1077edb?videoWidth=720&videoHeight=405&controlsVisibleOnLoad=true&autoPlay=true&popover=true" class="wistia-popover[width=720,height=405,playerColor=#636155]"><img class="bio_video" title="Chip - Lead Apple Guy's Video Bio" alt="Chip - Lead Apple Guy's Video Bio" src="images/about/video.png" /></a><script charset="ISO-8859-1" src="http://fast.wistia.com/static/popover-v1.js"></script>
+<a id="video_bio_link_chip" href="http://fast.wistia.com/embed/iframe/a0e1077edb?videoWidth=720&videoHeight=405&controlsVisibleOnLoad=true&autoPlay=true&popover=true" class="wistia-popover[width=720,height=405,playerColor=#636155]"><button class="video_bio_button" title="Chip - Lead Apple Guy's Video Bio" alt="Chip - Lead Apple Guy's Video Bio" />VIDEO BIO</button></a><script charset="ISO-8859-1" src="http://fast.wistia.com/static/popover-v1.js"></script>
 </div></div>
 
 <div class="bio" id="jay">
@@ -33,7 +33,7 @@ North Wales, PA</p>
 <p>Lead Apple Guy<br />West Chester, PA</p>
 
 <blockquote class="info">Jaguar, Panther, Tiger, Leopard, Snow Leopard, Cougar, Bobcat, Cheetah, Lynx, Bengal - we'll fix whatever Apple throws our way.</blockquote>
-<a id="video_bio_link_jay" href="http://fast.wistia.com/embed/iframe/ea1af3c16e?videoWidth=702&videoHeight=395&controlsVisibleOnLoad=true&autoPlay=true&popover=true" class="wistia-popover[width=702,height=395,playerColor=#636155]"><img class="bio_video" title="Jay - Lead Apple Guy's Video Bio" alt="Jay - Lead Apple Guy's Video Bio" src="images/about/video.png" /></a><script charset="ISO-8859-1" src="http://fast.wistia.com/static/popover-v1.js"></script>
+<a id="video_bio_link_jay" href="http://fast.wistia.com/embed/iframe/ea1af3c16e?videoWidth=702&videoHeight=395&controlsVisibleOnLoad=true&autoPlay=true&popover=true" class="wistia-popover[width=702,height=395,playerColor=#636155]"><button class="video_bio_button" title="Chip - Lead Apple Guy's Video Bio" alt="Chip - Lead Apple Guy's Video Bio" />VIDEO BIO</button></a><script charset="ISO-8859-1" src="http://fast.wistia.com/static/popover-v1.js"></script>
 </div></div>
 <div id="clear" style="clear:both;"></div>
 
--- includes/nav.php
+++ includes/nav.php
@@ -2,10 +2,14 @@
 <!--Navigation Start-->
 <div id="navbar">
 <ul>
-<li id="home" title="Home | Those Apple Guys"><a href="../"></a></li>
-<li id="about" title="About Those Apple Guys"><a href="about"></a></li>
-<li id="services" title="Services | Those Apple Guys"><a href="services"></a></li>
-<li id="contact" title="Contact Those Apple Guys"><a href="contact"></a></li>
+<li title="Home | Those Apple Guys">
+  <a href="../">HOME</a>
+</li><li title="About Those Apple Guys">
+  <a href="about">ABOUT</a>
+</li><li title="Services | Those Apple Guys">
+  <a href="services">SERVICES</a>
+</li><li title="Contact Those Apple Guys">
+  <a href="contact">CONTACT</a></li>
 </ul>
 <h2>Apple Computer Repair and Support</h2>
 </div>
