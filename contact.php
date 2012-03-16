542ab2d7c3fd55a279351431911199edfbc6cac7
Jeff Vincent <jeff@wistia.com>
2012-03-16 14:59:00 -0400

    contact page updates, looking sick

--- contact.php
+++ contact.php
@@ -11,33 +11,48 @@
 
 <!--Contact Start-->
 <div id="contact_locations">
+
 <div class="contact_bubbles" id="ambler">
+
 <h2>North Wales, PA</h2>
-<a href="tel:+1-215-645-4410" title="Call Chip - North Wales, PA Mac Specialist" target="_blank"><img title="Call Chip - North Wales, PA Mac Specialist" alt="Call Chip - North Wales, PA Mac Specialist" src="../images/contact/a_phone.png" /></a>
+<div class="big_phone">
+<a href="tel:+1-215-645-4410" title="Call Chip - North Wales, PA Mac Specialist" target="_blank"><img title="Call Chip - North Wales, PA Mac Specialist" alt="Call Chip - North Wales, PA Mac Specialist" src="images/contact/a_phone.png" /></a>
+</div>
+<div class="contact_deets">
 <h3>Chip</h3>
+
 <p><a title="508 S. Sumneytown Pike, North Wales PA 19454" target="_blank" href="<?php include('includes/ambler.php'); ?>">508 S. Sumneytown Pike<br />
-North Wales, PA 19454</a></p>
-<p>(215) 645-4410</p>
+North Wales, PA 19454</a><br />
+<span class="pc">(215) 645-4410</span></p>
 <a href="aim:goim?screenname=ThoseAppleGuysA&message=Hey+Chip+- " title="Instant Message Chip - North Wales, PA Mac Specialist" target="_blank"><img id="ichat" title="Instant Message Chip - Ambler, PA Mac Specialist" alt="Instant Message Chip - North Wales, PA Mac Specialist" src="http://big.oscar.aol.com/ThoseAppleGuysA?on_url=http://www.thoseappleguys.com/images/contact/ichat.png&off_url=http://www.thoseappleguys.com/images/contact/ichataway.png" /></a>
-<h4>Email Chip:</h4>
-<a href="mailto:chip@thoseappleguys.com" title="Email Chip - North Wales, PA Mac Specialist"><p>chip@thoseappleguys.com</p></a>
-<h4 id="general">General Inquires:</h4>
-<a href="mailto:info@thoseappleguys.com" title="General Inquires for Those Apple Guys"><p>info@thoseappleguys.com</p></a>
+<p><h4>Email Chip:</h4>
+<a href="mailto:chip@thoseappleguys.com" title="Email Chip - North Wales, PA Mac Specialist">chip@thoseappleguys.com</a></p>
+<p><h4 id="general">General Inquiries:</h4>
+<a href="mailto:info@thoseappleguys.com" title="General Inquires for Those Apple Guys">info@thoseappleguys.com</a></p>
+</div>
+<div id="clearfix" style="clear:both;"></div>
 </div>
 
 <div class="contact_bubbles" id="westchester">
 <h2>West Chester, PA</h2>
-<a href="tel:+1-610-486-6165" title="Call Jay - West Chester, PA Mac Specialist" target="_blank"><img title="Call Jay - West Chester, PA Mac Specialist" alt="Call Jay - West Chester, PA Mac Specialist" src="../images/contact/wc_phone.png" /></a>
+
+<div class="big_phone">
+  <a href="tel:+1-610-486-6165" title="Call Jay - West Chester, PA Mac Specialist" target="_blank"><img title="Call Jay - West Chester, PA Mac Specialist" alt="Call Jay - West Chester, PA Mac Specialist" src="images/contact/wc_phone.png" /></a>
+</div>
+
+<div class="contact_deets">
 <h3>Jay</h3>
 <p><a title="101 Everest Circle West Chester, PA 19382" target="_blank" href="<?php include('includes/westchester.php'); ?>">101 Everest Circle<br/>
-West Chester, PA 19382</a></p>
-<p>(610) 486-6165</p>
+West Chester, PA 19382</a><br />
+<span class="pc">(610) 486-6165</span></p>
 <a href="aim:goim?screenname=ThoseAppleGuysWC&message=Hey+Jay+-" title="Instant Message Jay - West Chester, PA Mac Specialist" target="_blank"><img id="ichat" title="Instant Message Jay - West Chester, PA Mac Specialist" alt="Instant Message Jay - West Chester, PA Mac Specialist" src="http://big.oscar.aol.com/ThoseAppleGuysWC?on_url=http://www.thoseappleguys.com/images/contact/ichat.png&off_url=http://www.thoseappleguys.com/images/contact/ichataway.png" /></a>
-<h4>Email Jay:</h4>
-<a href="mailto:jay@thoseappleguys.com" title="Email Jay - West Chester, PA Mac Specialist"><p>jay@thoseappleguys.com</p></a>
-<h4 id="general">General Inquires:</h4>
-<a href="mailto:info@thoseappleguys.com" title="General Inquires for Those Apple Guys"><p>info@thoseappleguys.com</p></a>
+<p><h4>Email Jay:</h4>
+<a href="mailto:jay@thoseappleguys.com" title="Email Jay - West Chester, PA Mac Specialist">jay@thoseappleguys.com</a></p>
+<p><h4 id="general">General Inquiries:</h4>
+<a href="mailto:info@thoseappleguys.com" title="General Inquires for Those Apple Guys">info@thoseappleguys.com</a></p>
 </div>
+</div>
+
 <div id="clearboth" style="clear: both;"></div>
 </div>
 
